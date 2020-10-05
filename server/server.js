c27421510ee6bbc40a72b8e4eca13d9872f95c98
Pastor Durán <pjduran@falabella.cl>
2020-10-05 20:33:54 -0300

    add acces control policies

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // parse application/x-www-form-urlencoded - Implementación de middleware
 app.use(bodyParser.urlencoded({ extended: false }))
  
