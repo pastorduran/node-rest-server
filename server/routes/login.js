f1764af9ae5491c7b23a54b513bb60a03a8260f9
Pastor Durán <pjduran@falabella.cl>
2020-12-08 13:51:29 -0300

    actualizacion de funcionalidad login

--- server/routes/login.js
+++ server/routes/login.js
@@ -21,7 +21,7 @@ app.post('/login', (req, res) => {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: '(Usuario) o contraseña incorrectos'
+                    message: 'Usuario o contraseña incorrectos'
                 }
             });
         };
@@ -30,7 +30,7 @@ app.post('/login', (req, res) => {
             return res.status(400).json({
                 ok: false,
                 err: {
-                    message: 'Usuario o (contraseña) incorrectos'
+                    message: 'Usuario o contraseña incorrectos'
                 }
             });
         };
